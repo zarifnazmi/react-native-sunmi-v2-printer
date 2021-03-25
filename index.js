01e89dd780b6553afb2236187035317572c256b4
Pavinthan <13897936+pavinthan@users.noreply.github.com>
2021-03-25 15:44:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { NativeModules } from 'react-native';
 
 const SunmiV2Printer = NativeModules.SunmiV2Printer;
+const CashDrawer = NativeModules.CashDrawer;
 
-module.exports = SunmiV2Printer;
+module.exports = {SunmiV2Printer, CashDrawer};
