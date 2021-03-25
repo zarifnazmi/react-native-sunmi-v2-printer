d5745f7f99e92f09d7540631d3dac7d821ae83f8
Pavinthan <13897936+pavinthan@users.noreply.github.com>
2021-03-25 15:43:08 +0530

    Add files via upload

---
+++ android/src/main/java/com/sunmi/v2/printer/CashDrawer.java
@@ -0,0 +1,79 @@
+package com.sunmi.v2.printer;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+import android.os.RemoteException;
+
+import com.facebook.react.bridge.ReactMethod;
+
+import woyou.aidlservice.jiuiv5.IWoyouService;
+import woyou.aidlservice.jiuiv5.ICallback;
+
+import com.facebook.react.bridge.Callback;
+
+import android.content.ServiceConnection;
+import android.content.ComponentName;
+import android.os.IBinder;
+import android.content.Intent;
+import android.content.Context;
+
+
+public class CashDrawer extends ReactContextBaseJavaModule {
+
+    public static ReactApplicationContext reactApplicationContext = null;
+    private IWoyouService woyouService;
+
+    private ServiceConnection connService = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            woyouService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            woyouService = IWoyouService.Stub.asInterface(service);
+        }
+    };
+
+    CashDrawer(ReactApplicationContext context) {
+        super(context);
+
+        reactApplicationContext = context;
+        Intent intent = new Intent();
+        intent.setPackage("woyou.aidlservice.jiuiv5");
+        intent.setAction("woyou.aidlservice.jiuiv5.IWoyouService");
+        context.startService(intent);
+        context.bindService(intent, connService, Context.BIND_AUTO_CREATE);
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "CashDrawer";
+    }
+
+    @ReactMethod
+    public void open(Callback callBack) {
+        final IWoyouService service = woyouService;
+        if (service != null) {
+            byte[] mOpenDrawerCommand = new byte[5];
+            mOpenDrawerCommand[0] = 0x10;
+            mOpenDrawerCommand[1] = 0x14;
+            mOpenDrawerCommand[2] = 0x00;
+            mOpenDrawerCommand[3] = 0x00;
+            mOpenDrawerCommand[4] = 0x00;
+
+            try {
+                service.sendRAWData(mOpenDrawerCommand, null);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+                callBack.invoke(false);
+            }
+        } else {
+            callBack.invoke(service, woyouService);
+        }
+    }
+}
